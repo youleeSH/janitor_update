ebcdb85af66ec84f9e64894400fc3afef5dbae41
Daeun522 <sde3914@gmail.com>
2025-07-04 13:47:34 +0900

    runShiny.R 수정 및 shiny.R 상위폴더 이동

--- R/runShiny.R
+++ R/runShiny.R
@@ -1,11 +1,5 @@
-#' Run the Beautiful Shiny Dashboard
-#'
-#' Launches the integrated data cleaning & visualization dashboard.
-#' @export
 run_beautiful_dashboard <- function() {
-  # 'shiny.R' 파일이 패키지 루트 또는 inst 아래 존재하는 경우 자동 탐색
-  app_file <- system.file("R/shiny.R", package = "janitor_update")
+  app_file <- system.file("shiny.R", package = "janitor_update")
   if (app_file == "") stop("Could not find shiny.R file. Please reinstall or fix the package.")
-  
-  source(app_file, local = TRUE)  # shinyApp()가 정의되어 있다고 가정
+  source(app_file, local = TRUE)
 }
