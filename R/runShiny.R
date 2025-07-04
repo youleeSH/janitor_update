d30bcf7edf91a8c526e67e7a0cbb0b184a90c6c3
Daeun522 <sde3914@gmail.com>
2025-07-04 13:14:50 +0900

    runShiny.R 수정

--- R/runShiny.R
+++ R/runShiny.R
@@ -3,7 +3,9 @@
 #' Launches the integrated data cleaning & visualization dashboard.
 #' @export
 run_beautiful_dashboard <- function() {
-  app_file <- system.file("shiny", "app.R", package = "mypackage") # mypackage는 패키지명
-  if (app_file == "") stop("Could not find Shiny app file. Please reinstall the package.")
-  shiny::runApp(app_file, display.mode = "normal")
+  # 'shiny.R' 파일이 패키지 루트 또는 inst 아래 존재하는 경우 자동 탐색
+  app_file <- system.file("shiny.R", package = "janitor")  # ← 패키지명 주의
+  if (app_file == "") stop("Could not find shiny.R file. Please reinstall or fix the package.")
+  
+  source(app_file, local = TRUE)  # shinyApp()가 정의되어 있다고 가정
 }
